5a42c5ab76b9d24f84273f5496af48a178458dee
Eyasu Weld <Eyasuweld@gmail.com>
2020-07-22 22:52:56 -0500

    Nested Routes

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -7,7 +7,8 @@ import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Users from "./components/admin/users";
 
 class App extends Component {
   render() {
@@ -22,7 +23,9 @@ class App extends Component {
           <Route path="/products" component={Products} />
           <Route path="/posts/:year?/:month?" component={Posts} />
           <Route path="/admin" component={Dashboard} />
-          <Route path="/" component={Home} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/not-found" />
         </Switch>
       </div>
     );
