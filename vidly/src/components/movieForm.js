f32b462cdf08b6cfbcd972adabde49d95cfc00d3
Eyasu Weld <Eyasuweld@gmail.com>
2020-07-23 16:50:25 -0500

    Routs

--- vidly/src/components/movieForm.js
+++ vidly/src/components/movieForm.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-const MovieForm = ({ location }) => {
-  return <h1>Movie Form </h1>;
-};
+class MovieForm extends Component {
+  state = {};
+
+  handleSave = () => {
+    this.props.history.replace("/movies");
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Movie Form {this.props.match.params.id} </h1>
+        <button className="btn btn-secondary btn-sm" onClick={this.handleSave}>
+          Save
+        </button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MovieForm;
