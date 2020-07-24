d873e8576f36a55c1c622eea96bcacb6aa71ca09
Eyasu Weld <Eyasuweld@gmail.com>
2020-07-24 10:26:45 -0500

    Controlled elements

--- vidly/src/components/login.jsx
+++ vidly/src/components/login.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("submitted");
   };
 
   render() {
+    let { account } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" className="form-control" />
+          <input
+            value={account.username}
+            onChange={this.handleChange}
+            name="username"
+            id="username"
+            type="text"
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" className="form-control" />
+          <input
+            value={account.password}
+            onChange={this.handleChange}
+            name="password"
+            id="password"
+            type="password"
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
