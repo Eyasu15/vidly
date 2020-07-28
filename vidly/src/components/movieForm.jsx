b40237920c96468a49fa19ef72b6a3a34a432f6c
Eyasu Weld <Eyasuweld@gmail.com>
2020-07-27 22:21:12 -0500

    Refactored form component

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -2,71 +2,77 @@ import React from "react";
 import Form from "./common/form";
 import Joi, { errors } from "joi-browser";
 import { saveMovie, getMovie } from "./services/fakeMovieService";
+import { getGenres } from "./services/fakeGenreService";
 
 class MovieForm extends Form {
   state = {
     data: {
-      id: "",
       title: "",
-      genre: "",
+      genreId: "",
       numberInStock: "",
       dailyRentalRate: "",
     },
+    genres: [],
     errors: {},
   };
 
   schema = {
+    _id: Joi.string(),
     title: Joi.string().required().label("Title"),
-    genre: Joi.string().required().label("Genre"),
+    genreId: Joi.string().required().label("Genre"),
     numberInStock: Joi.number()
+      .required()
       .min(0)
       .max(100)
-      .required()
       .label("Number in Stock"),
-    dailyRentalRate: Joi.number().min(0).max(10).required().label("Rate"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate"),
   };
 
   componentDidMount() {
-    const movie = getMovie(this.props.match.params.id);
-    if (!movie) return;
+    const genres = getGenres();
+    this.setState({ genres });
 
-    const data = {
-      id: movie._id,
+    const movieId = this.props.match.params.id;
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+
+  mapToViewModel(movie) {
+    return {
+      _id: movie._id,
       title: movie.title,
-      genre: movie.genre.name,
+      genreId: movie.genre._id,
       numberInStock: movie.numberInStock,
       dailyRentalRate: movie.dailyRentalRate,
     };
-
-    this.setState({ data });
   }
 
   doSubmit = () => {
-    let { title, genre, numberInStock, dailyRentalRate } = this.state.data;
-    genre = { _id: genre, name: "" };
-    const movie = { title, genre, numberInStock, dailyRentalRate };
-    console.log(movie);
-    saveMovie(movie);
-    this.props.history.replace("/movies");
+    saveMovie(this.state.data);
+
+    this.props.history.push("/movies");
   };
+
   render() {
-    const genres = [
-      { name: "Action", value: "5b21ca3eeb7f6fbccd471818" },
-      { name: "Comedy", value: "5b21ca3eeb7f6fbccd471814" },
-      { name: "Thriller", value: "5b21ca3eeb7f6fbccd471820" },
-    ];
-    console.log(this.state.errors);
     return (
-      <React.Fragment>
-        <h1>Movie Form </h1>
+      <div>
+        <h1>Movie Form</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("title", "Title")}
-          {this.renderInput("genre", "Genre", "select", genres)}
-          {this.renderInput("numberInStock", "Number in Stock")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
           {this.renderInput("dailyRentalRate", "Rate")}
           {this.renderButton("Save")}
         </form>
-      </React.Fragment>
+      </div>
     );
   }
 }
