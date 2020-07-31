21a0e33242180c06cf4380f2d01fa60f49ab2e81
Eyasu Weld <Eyasuweld@gmail.com>
2020-07-30 23:26:57 -0500

    Authentication

--- router-app/src/components/navbar.jsx
+++ router-app/src/components/navbar.jsx
@@ -7,6 +7,15 @@ const NavBar = (props) => {
       <li>
         <Link to="/">Home</Link>
       </li>
+      {/* <li>
+        <Link to="/products">Products</Link>
+      </li>
+      <li>
+        <Link to="/posts/2018/06">Posts</Link>
+      </li>
+      <li>
+        <Link to="/admin">Admin</Link>
+      </li> */}
     </ul>
   );
 };
