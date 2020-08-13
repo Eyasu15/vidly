1cec9fdeb89a60a0c8ccb76e3cdcf96e42ed6822
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-12 22:01:10 -0500

    Refactoring bi-directional dependency

--- src/components/services/userService.js
+++ src/components/services/userService.js
@@ -3,7 +3,7 @@ import { apiUrl } from "../utils/config.json";
 import jwtDecode from "jwt-decode";
 
 const usersUrl = apiUrl + "users";
-const tokenKey = "token";
+const token = "token";
 http.setJwt(token);
 function registerUser(user) {
   return http.post(usersUrl + "/register", user);
@@ -14,24 +14,24 @@ async function login(email, password) {
     email,
     password,
   });
-  localStorage.setItem(tokenKey, jwt);
+  localStorage.setItem(token, jwt);
 }
 
 function loginWithJwt(jwt) {
-  localStorage.setItem(tokenKey, jwt);
+  localStorage.setItem(token, jwt);
 }
 
 function logout() {
-  localStorage.removeItem(tokenKey);
+  localStorage.removeItem(token);
 }
 
 function getCurrentUser() {
   try {
-    const jwt = localStorage.getItem(tokenKey);
+    const jwt = localStorage.getItem(token);
     return jwtDecode(jwt);
   } catch (ex) {
     return null;
   }
 }
 
-export { registerUser, login, logout, getCurrentUser, loginWithJwt, getHeader };
+export { registerUser, login, logout, getCurrentUser, loginWithJwt };
