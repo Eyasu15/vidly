8dc8d2210dc69e3ace4f85b1e7238b068ad2f5e1
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-14 15:21:28 -0500

    ProtectedRoute

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+
+const ProtectedRoute = (props) => {
+  <Route
+    path={props.path}
+    render={(props) => {
+      if (!props.user) return <Redirect to={props.login} />;
+    }}
+  />;
+};
