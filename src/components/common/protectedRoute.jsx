5c7ef9704d875f025b2c46ad33bf489eb3123548
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-15 23:46:23 -0500

    Protected Route

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -7,7 +7,15 @@ const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
     <Route
       {...rest}
       render={(props) => {
-        if (!getCurrentUser()) return <Redirect to={props.login} />;
+        if (!getCurrentUser())
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: props.location },
+              }}
+            />
+          );
         return Component ? <Component {...props} /> : render(props);
       }}
     />
