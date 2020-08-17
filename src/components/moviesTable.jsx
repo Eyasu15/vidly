a9c573ed6f3b83cf3a030e8487f70d1e3abc1b05
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-16 23:33:51 -0500

    Delete button

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import { render } from "@testing-library/react";
 import Table from "./common/table";
-import { Route } from "react-router-dom";
 
 class MoviesTable extends Component {
   columns = [
