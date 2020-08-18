2afff36257c2e73d2e36744b91a773bd2e51fbc0
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-17 23:07:24 -0500

    refactor movies components

--- src/components/customerComponents/customerForm.jsx
+++ src/components/customerComponents/customerForm.jsx
@@ -5,7 +5,7 @@ import Joi from "joi-browser";
 class CustomerForm extends Form {
   state = {
     id: "",
-    customerName: "",
+    name: "",
     phone: "",
     isGold: "",
     errors: {},
@@ -13,12 +13,16 @@ class CustomerForm extends Form {
 
   schema = {
     id: Joi.number().required(),
-    customerName: Joi.string().required().label("Name"),
+    name: Joi.string().required().label("Name"),
     phone: Joi.string()
       .trim()
       .regex(/^[0-9]{7,10}$/)
       .required()
       .label("Phone"),
+  };
+
+  doSubmit = () => {
+    console.log("submitted");
   };
   render() {
     const customerTier = ["Gold", "Regular"];
@@ -26,7 +30,7 @@ class CustomerForm extends Form {
       <div>
         <h1>Customer Form</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("customerName", "Name")}
+          {this.renderInput("name", "Name")}
           {this.renderSelect("isGold", "Level", customerTier)}
           {this.renderInput("phone", "Phone")}
           {this.renderButton("Save")}
