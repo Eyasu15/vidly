6b990da5e6bd8a1f1b0ddab3ad408d9b3621ce04
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-26 22:10:54 -0500

    rentals service

--- src/components/services/rentalsService.js
+++ src/components/services/rentalsService.js
@@ -2,3 +2,7 @@ import http from "./httpService";
 import { apiUrl } from "../utils/config.json";
 
 const rentalUrl = apiUrl + "rentals";
+
+function getAllRentals() {
+  return http.get(rentalUrl);
+}
