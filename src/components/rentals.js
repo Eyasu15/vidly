5163dfdcc5f0e345b6be0e21f67c137e269df0e6
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-26 22:16:16 -0500

    rentals table

--- src/components/rentals.js
+++ src/components/rentals.js
@@ -1,10 +1,14 @@
 import React,{Component} from "react";
+import rentalsService from "./services/rentalsService";
 
 class Rentals extends Component {
-  state = {  }
+  state = { 
+    rentals: []
+   }
 
   async componentDidMount() {
-    const {data: rentals} = await
+    const {data: rentals} = await rentalsService.getAllRentals();
+    this.setState({rentals})
   }
 
   render() { 
