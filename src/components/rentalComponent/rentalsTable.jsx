5615a451a5b4cacf2c2cd606d2cb608ce9861056
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-27 23:21:06 -0500

    Rentals

--- src/components/rentalComponent/rentalsTable.jsx
+++ src/components/rentalComponent/rentalsTable.jsx
@@ -4,7 +4,9 @@ class RentalsTable extends Component {
     state = [
         {label:"Movie", path:"movie",clicked:false},
         {label:"Customer", path:"customer",clicked:false},
-        {label:"Date Out", path:"dateOut",clicked:false}]
+        {label:"Date Out", path:"dateOut",clicked:false},
+        {label:"Return Date", path:"dateReturn",clicked:false},
+        {label:"Fee", path:"rentalFee",clicked:false}]
     render() { 
         return (  );
     }
