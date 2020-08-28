99fbc5bd3e262b0f6e1d288334207c54bab8b062
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-27 23:22:11 -0500

    Rentals

--- src/components/rentalComponent/rentalsTable.jsx
+++ src/components/rentalComponent/rentalsTable.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 class RentalsTable extends Component {
-    state = [
+    columns = [
         {label:"Movie", path:"movie",clicked:false},
         {label:"Customer", path:"customer",clicked:false},
         {label:"Date Out", path:"dateOut",clicked:false},
         {label:"Return Date", path:"dateReturn",clicked:false},
-        {label:"Fee", path:"rentalFee",clicked:false}]
-    render() { 
+        {label:"Fee", path:"rentalFee",clicked:false}];
+    
+    
+        render() { 
         return (  );
     }
 }
